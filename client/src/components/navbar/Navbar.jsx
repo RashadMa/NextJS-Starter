db50bba6bc99dc35fd2039d2bed52f51fed1c857
rashadma <rashadkhm@code.edu.az>
2024-02-17 16:05:27 +0200

    Home page is done

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Links from './links/Links'
 import styles from "./navbar.module.scss"
+import Link from 'next/link'
 
 const Navbar = () => {
       return (
             <div className={styles.container}>
-                  <div className={styles.logo}>
-                        Logo
-                  </div>
+                  <Link href="/" className={styles.logo}>Logo</Link>
                   <div className='links'>
                         <Links />
                   </div>
